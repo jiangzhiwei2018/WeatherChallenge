c0466902d9cb29511c351608bfe7a50c450c72fa
jzw <804034739@qq.com>
2022-05-19 18:28:57 +0800

    sgm

--- configs/models/weather_model.py
+++ configs/models/weather_model.py
@@ -1,9 +1,9 @@
-model_work_name = 'BasicWeather_UnetGenerator_DCNv2_interpolate_MSELoss'
+model_work_name = 'BasicWeather_UnetGenerator_DCNv2_interpolate_MSELoss_numdown_2'
 
 model = dict(
     type="BasicWeatherModel",
     generator=dict(type="BasicWeather",
-                   generator_backbone_cfg=dict(type="UnetGenerator", num_down=8),
+                   generator_backbone_cfg=dict(type="UnetGenerator", num_down=2),
                    upsample_type="interpolate", conv_cfg=dict(type='DCNv2')),
     pixel_loss=dict(
         type="MSELoss"  # smooth L1
